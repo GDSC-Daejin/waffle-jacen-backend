336a1a68d0c617e4840d72ddd7b24b25e5c3ac74
windowdong11 <windowdong11@naver.com>
2022-10-24 21:57:48 +0900

    api 테스트용 /ping endpoint 삭제

--- todo/src/main/java/org/jacen/todo/controller/TodoController.java
+++ todo/src/main/java/org/jacen/todo/controller/TodoController.java
@@ -14,48 +14,40 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TodoController {
 
-  @Autowired
-  private TodoService repository;
+    @Autowired
+    private TodoService repository;
 
-  @GetMapping("/ping")
-  public String ping() {
-    return "pong";
-  }
-  // 투두 리스트를 가져오기
-  @GetMapping("/todo/list")
-  public List<TodoDto> list(@RequestParam(value="page", required=false) Integer page) {
-    if (page == null || page < 1) {
-      page = 1;
+    // 투두 리스트를 가져오기
+    // TODO : 페이징 구현
+    // @RequestParam(value = "page", required = false, defaultValue = "1") Integer page
+    @GetMapping("/todo/list")
+    public List<TodoDto> list() {
+        return ObjectMapperUtils.mapAll(repository.findAll(), TodoDto.class);
     }
-    return ObjectMapperUtils.mapAll(repository.findAll(), TodoDto.class);
-  }
 
-//   id로 투두 세부정보 가져오기
-   @GetMapping("/todo/{id}")
-   public TodoDto getTodoById(@PathVariable String id) {
-     Optional<Todo> todo = repository.findById(id);
-     if (todo.isPresent()) {
-       return ObjectMapperUtils.map(todo.get(), TodoDto.class);
-     }
-     return null;
-   }
+    // id로 투두 세부정보 가져오기
+    @GetMapping("/todo/{id}")
+    public TodoDto getTodoById(@PathVariable String id) {
+        Optional<Todo> todo = repository.findById(id);
+        return todo.map(value -> ObjectMapperUtils.map(value, TodoDto.class)).orElse(null);
+    }
 
-   // 투두 추가
-   @PostMapping("/todo")
-   public TodoDto postMethodName(@RequestBody Todo entity) {
-       return ObjectMapperUtils.map(repository.addTodo(entity), TodoDto.class);
-   }
+    // 투두 추가
+    @PostMapping("/todo")
+    public TodoDto postMethodName(@RequestBody Todo entity) {
+        return ObjectMapperUtils.map(repository.addTodo(entity), TodoDto.class);
+    }
 
-   // 투두 수정
-   @PutMapping("/todo/{id}")
-   public TodoDto updateTodoById(@PathVariable String id, @RequestBody TodoDto todo) {
-      return ObjectMapperUtils.map(repository.updateById(id, ObjectMapperUtils.map(todo, Todo.class)), TodoDto.class);
-   }
+    // 투두 수정
+    @PutMapping("/todo/{id}")
+    public TodoDto updateTodoById(@PathVariable String id, @RequestBody TodoDto todo) {
+        return ObjectMapperUtils.map(repository.updateById(id, ObjectMapperUtils.map(todo, Todo.class)), TodoDto.class);
+    }
 
-   //투두 삭제
-   @DeleteMapping("/todo/{id}")
-   public String deleteTodoById(@PathVariable String id) {
-     repository.deleteById(id);
-     return "Deleted";
-   }
+    // 투두 삭제
+    @DeleteMapping("/todo/{id}")
+    public String deleteTodoById(@PathVariable String id) {
+        repository.deleteById(id);
+        return "Deleted";
+    }
 }
